795bf04ae74e5c4eee582687b3b5c1fd643f1002
karino <karino@kazaoki.jp>
2019-05-30 10:42:16 +0900

    [UPDATE] php notice 対応

--- html/index.php
+++ html/index.php
@@ -29,9 +29,9 @@ extract($_POST);
 <dl>
   <dt>性別 *</dt>
   <dd>
-    <label><input type="radio" name="gender" value="男"<?php echo checked($gender, '男') ?>>男</label>
+    <label><input type="radio" name="gender" value="男"<?php echo checked(@$gender, '男') ?>>男</label>
     &nbsp;
-    <label><input type="radio" name="gender" value="女"<?php echo checked($gender, '女') ?>>女</label>
+    <label><input type="radio" name="gender" value="女"<?php echo checked(@$gender, '女') ?>>女</label>
     <p data-validon-errorholder="gender"></p>
   </dd>
 </dl>
@@ -40,22 +40,22 @@ extract($_POST);
   <dd>
     <select name="age">
       <option value="">一つ選択してください。</option>
-      <option value="10代未満"<?php echo selected($age, '10代未満') ?>>10代未満</option>
-      <option value="20代"<?php echo selected($age, '20代') ?>>20代</option>
-      <option value="30代"<?php echo selected($age, '30代') ?>>30代</option>
-      <option value="40代"<?php echo selected($age, '40代') ?>>40代</option>
-      <option value="50代"<?php echo selected($age, '50代') ?>>50代</option>
-      <option value="60代"<?php echo selected($age, '60代') ?>>60代</option>
-      <option value="70代"<?php echo selected($age, '70代') ?>>70代</option>
-      <option value="80代"<?php echo selected($age, '80代') ?>>80代</option>
-      <option value="90代以上"<?php echo selected($age, '90代以上') ?>>90代以上</option>
+      <option value="10代未満"<?php echo selected(@$age, '10代未満') ?>>10代未満</option>
+      <option value="20代"<?php echo selected(@$age, '20代') ?>>20代</option>
+      <option value="30代"<?php echo selected(@$age, '30代') ?>>30代</option>
+      <option value="40代"<?php echo selected(@$age, '40代') ?>>40代</option>
+      <option value="50代"<?php echo selected(@$age, '50代') ?>>50代</option>
+      <option value="60代"<?php echo selected(@$age, '60代') ?>>60代</option>
+      <option value="70代"<?php echo selected(@$age, '70代') ?>>70代</option>
+      <option value="80代"<?php echo selected(@$age, '80代') ?>>80代</option>
+      <option value="90代以上"<?php echo selected(@$age, '90代以上') ?>>90代以上</option>
     </select>
   </dd>
 </dl>
 <dl>
   <dt>お問い合わせ内容 *</dt>
   <dd>
-    <textarea name="content" cols="30" rows="10"><?php echo h($content) ?></textarea>
+    <textarea name="content" cols="30" rows="10"><?php echo h(@$content) ?></textarea>
   </dd>
 </dl>
 <button>確認画面へ</button>
